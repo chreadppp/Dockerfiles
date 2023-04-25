629a881623369d6d358405273dd9da03b5e21fd8
chreadppp <corray8270@gmail.com>
2023-04-25 09:48:50 +0800

    Update krew_plugin.sh

--- kubetools/krew_plugin.sh
+++ kubetools/krew_plugin.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+
+
+
+LATEST_VERSION(){
+local REPOS=$1
+curl -s https://api.github.com/repos/$REPOS/releases/latest |grep -oP '"tag_name": "\K(.*)(?=")'
+}
+
+
 echo '[STEP 1] Creating directory for kubectl config'
 mkdir /root/.kube
 
 echo '[STEP 2] Installing Krew'
 (
   set -x; cd "$(mktemp -d)" &&
-  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-  KREW="krew-${OS}_${ARCH}" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/$(krew-${OS}_${ARCH}).tar.gz" &&
   tar zxvf "${KREW}.tar.gz" &&
   ./"${KREW}" install krew
 )
@@ -24,15 +33,18 @@ echo '[STEP 3] Installing kubectx and kubens - quickly switch kubernetes context
 
 echo '[STEP 4] Installing stern'
 (
-    wget https://github.com/wercker/stern/releases/download/1.11.0/stern_linux_amd64 && \
-    chmod +x stern_linux_amd64 && \
-    mv stern_linux_amd64 /usr/local/bin/stern
+    TAG=$(LATEST_VERSION "chenjiandongx/kubectl-images")
+    TAD=$(echo $TAG | awk -F "v" '{print$2}')
+    wget -c  https://github.com/stern/stern/releases/download/$TAG/stern_$TAD_linux_amd64.tar.gz -O - | tar -xz && \
+    chmod +x stern && \
+    mv stern /usr/local/bin/stern
 )
 
 echo '[STEP 5] Installing kubectl-images plugin'
 (
     set -x &&
-    wget -c https://github.com/chenjiandongx/kubectl-images/releases/download/v0.3.6/kubectl-images_linux_amd64.tar.gz -O - | tar -xz &&
+    TAG=$(LATEST_VERSION "chenjiandongx/kubectl-images")
+    wget -c https://github.com/chenjiandongx/kubectl-images/releases/download/$TAG/kubectl-images_linux_amd64.tar.gz -O - | tar -xz &&
     chmod +x kubectl-images &&
     mv kubectl-images /usr/local/bin/
 )
@@ -40,7 +52,8 @@ echo '[STEP 5] Installing kubectl-images plugin'
 echo '[STEP 6] IInstalling kubectl-neat plugin'
 (
     set -x &&
-    wget -c https://github.com/itaysk/kubectl-neat/releases/download/v2.0.3/kubectl-neat_linux_amd64.tar.gz  -O - | tar -xz &&
+    TAG=$(LATEST_VERSION "itaysk/kubectl-neat")
+    wget -c https://github.com/itaysk/kubectl-neat/releases/download/$TAG/kubectl-neat_linux_amd64.tar.gz  -O - | tar -xz &&
     chmod +x kubectl-neat &&
     mv kubectl-neat /usr/local/bin/
 )
@@ -48,7 +61,7 @@ echo '[STEP 6] IInstalling kubectl-neat plugin'
 echo '[STEP 7] IInstalling kubectl-iexec plugin'
 (
     # Get latest release
-    TAG=$(curl -s https://api.github.com/repos/gabeduke/kubectl-iexec/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+    TAG=$(LATEST_VERSION "gabeduke/kubectl-iexec")
 
     # Donwload and extract binary to /usr/local/bin
     wget -c  https://github.com/gabeduke/kubectl-iexec/releases/download/${TAG}/kubectl-iexec_${TAG}_${OS:-Linux}_x86_64.tar.gz -O - | tar -xz &&
